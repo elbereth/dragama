c274e5488253072c716bdea40faf626aeac6ea7f
Alexandre Devilliers <alexandre.devilliers@elberethzone.net>
2012-04-22 19:28:11 +0200

    Initial commit with GPLv3 headers & file
    Signed-off-by: Alexandre Devilliers <alexandre.devilliers@elberethzone.net>

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ dragama
 =======
 
 Dragon Game Manipulator is the logical step after Dragon UnPACKer: Manipulate games files, create patches, distribute your mods and hopefully much more!
+
+
+License:
+========
+Dragon Game resource Manager [DraGaMa] - A Game resource file manipulator
+Copyright (C) 2012  Alexandre Devilliers <alexandre.devilliers@adesysop.fr>
+                  Snail Mail: 23 Rue Van Craeynest - 18100 Vierzon - FRANCE
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ core/dragama_core.dpr
@@ -0,0 +1,37 @@
+// Dragon Game resource Manager [DraGaMa] - A Game resource file manipulator
+// Copyright (C) 2012  Alexandre Devilliers <alexandre.devilliers@adesysop.fr>
+//                  Snail Mail: 23 Rue Van Craeynest - 18100 Vierzon - FRANCE
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+library dragama_core;
+
+{ Important note about DLL memory management: ShareMem must be the
+  first unit in your library's USES clause AND your project's (select
+  Project-View Source) USES clause if your DLL exports any procedures or
+  functions that pass strings as parameters or function results. This
+  applies to all strings passed to and from your DLL--even those that
+  are nested in records and classes. ShareMem is the interface unit to
+  the BORLNDMM.DLL shared memory manager, which must be deployed along
+  with your DLL. To avoid using BORLNDMM.DLL, pass string information
+  using PChar or ShortString parameters. }
+
+uses
+  System.SysUtils,
+  System.Classes;
+
+{$R *.res}
+
+begin
+end.
